859201da1b5118a99950ef170146ed276f3296e1
Lennon Araujo <lennon_hih@hotmail.com>
2023-06-29 20:22:53 -0300

    apply rendering dinamic

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,31 +1,48 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
-import styles from './Post.module.css';
+import PropTypes from 'prop-types';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export function Post() {
+import styles from './Post.module.css';
+export function Post({author, content, publishedAt}) {
+  const publishedAtFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  })
+
+  const publishedAtRelativeNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/Lennon-Araujo.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Lennon Rodrigues</strong>
-            <span>QA Analyst</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='28 de Junho às 18:40' dateTime='2023-06-28 18:40:00'>Publicado há 1h</time>
+        <time title={publishedAtFormatted} dateTime={publishedAt.toISOString()}>{publishedAtRelativeNow}</time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href=''>jane.design/doctorcare</a></p>
-        <p>
-          <a href=''>#novoprojeto</a>{' '}
-          <a href=''>#nlw</a>{' '}
-          <a href=''>#rocketseat</a>
-        </p>
+        {content.map((cont, index) => {
+          if(cont.type === 'paragraph') {
+            return <p key={index}>{cont.content}</p>
+          }
+
+          if(cont.type === 'link') {
+            return <a key={index}><p>{cont.content}</p></a>
+          }
+
+          if(cont.type === 'hashtag') {
+            return <a key={index}>{cont.content + ' '}</a>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
@@ -46,4 +63,10 @@ export function Post() {
         
     </article>
   )
+}
+
+Post.propTypes ={
+  author: PropTypes.object,
+  content: PropTypes.array,
+  publishedAt: PropTypes.object
 }
